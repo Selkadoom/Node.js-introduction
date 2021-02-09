ef25bb9761ddd605b866e22b92324a374cb1af68
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-02-09 18:23:55 -0300

    09/02 - 18:22 Criando o servidor {Aqui eu criei o servidor que será usado como teste, criei uma constante que recebe o require http, em seguida, coloquei um let para receber o create server do http, onde ele recebe o requisito e a resposta, em seguida, apenas para teste, pedi um console log da url e do método. Após tudo isso, criei um server.listener que pega a entrada 3000, o ip e irei começar a contruir uma função para ele executar algo}

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-var oi = "hello world"
-//teste
+const http = require('http');
+
+let server = http.createServer((req, res) => {
+    console.log("URL:", req.url);
+    console.log("METHOD:", req.method);
+
+});
+
+server.listen(3000, '10.0.1.112', ()=>{
+
+    
+})
