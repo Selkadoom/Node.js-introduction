2457f1c7b9576845775c0df025ad0740780aff26
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-02-10 20:16:31 -0300

    10/02 - 20:15 - [Criando uma constante express que recebe os módulos do express, em seguida, um let é criado para receber a biblioteca express, carregando os módulos]

--- script.js
+++ script.js
@@ -1,34 +1,6 @@
-/*
-const http = require('http'); //carregou o módulo
+const express = require("express");
 
-let server = http.createServer((req, res) => { //criou o servidor no let
-    console.log("URL:", req.url); //todas as solicitações do servidor são mandadas pro req e a reposta pro res
-    console.log("METHOD:", req.method);
-res.end("Ok")
-});
-
-server.listen(3000, '10.0.1.112', ()=>{ // a variável mandada pelo servidor vai ser ouvida na porta 3000
-    //qualquer porta nao usada funciona, lembrando que o ip é o enredeço local
-
-    console.log("sevidor rodando")
-});
-*/
-//-----------------
-/*
-const http = require('http'); //carregou o módulo
-let server = http.createServer(function (request, response) { //criou o servidor no let
-    console.log("URL:", request.url); //todas as solicitações do servidor são mandadas pro req e a reposta pro res
-    console.log("METHOD:", request.method);
-    request.readableEnded("Ok")
-});
-server.listen(3000, '10.0.1.112', () => {
-    //qualquer porta nao usada funciona, lembrando que o ip é o enredeço local
-    console.log("servidor funfando");
-} );
-
-*/
-//ACIMA SÓ TEM OS ERRADOS
-const http = require("http");
+let app = express();
 
 let servidor = http.createServer(function (req, resp) {
   console.log("URL:", req.url); //todas as solicitações do servidor são mandadas pro req e a reposta pro res
@@ -54,19 +26,18 @@ let servidor = http.createServer(function (req, resp) {
 
       resp.end(
         JSON.stringify({
-          users: [{
-                name: "Reinaldo Ramos",
-                email: "Reinaldo_RAmos@gmail.com",
-                id: 1
-
-          }]
+          users: [
+            {
+              name: "Reinaldo Ramos",
+              email: "Reinaldo_RAmos@gmail.com",
+              id: 1,
+            },
+          ],
         })
       );
 
-    
-      
-      default:
-        resp.end(`
+    default:
+      resp.end(`
         <html>
            <head>
                <meta charset="utf -8">
@@ -77,7 +48,6 @@ let servidor = http.createServer(function (req, resp) {
         </html>
    `);
   }
- 
 });
 servidor.listen(3000, () => {
   console.log("funfando");
