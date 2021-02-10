476c22f9886d294ad98d6daae8a9243f6e230d42
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-02-10 17:58:05 -0300

    10/02 - 18:00 - [Aqui eu primeiramente arrumei o erro do res, pois o parâmetro da consulta era resp, , criei os 2 swtiches que caso recebam uma determinada rota, executam algo, aqui caso receba somente uma barra ele retorna um ola e caso receba a rota users ele retorna um objeto JSON, testado e funcionando, lembrando que é bom começar a memorizar o assunto rotas pois é muito importante para o node.js]

--- script.js
+++ script.js
@@ -37,22 +37,22 @@ let servidor = http.createServer(function (req, resp) {
     case "/":
       req.statusCode = 200; //quando o usuário conecta com o servidor ele retorna um status code
       //se for 200, a conexão deu certo
-      res.setHeader("Content-Type", "text/html"); //Aqui eu to passando o tipo do conteudo
+      resp.setHeader("Content-Type", "text/html"); //Aqui eu to passando o tipo do conteudo
       //e avisando que é Html
       //----------
 
-      res.end("<h1>Ola</h1>"); //pedindo pra mandar uma res que cria um h1 com Ola escito
+      resp.end("<h1>Ola</h1>"); //pedindo pra mandar uma res que cria um h1 com Ola escito
 
       break;
 
     case "/users":
       req.statusCode = 200; //quando o usuário conecta com o servidor ele retorna um status code
       //se for 200, a conexão deu certo
-      res.setHeader("Content-Type", "application/json"); //Aqui eu to passando o tipo do conteudo
+      resp.setHeader("Content-Type", "application/json"); //Aqui eu to passando o tipo do conteudo
       //e avisando que é Html
       //----------
 
-      res.end(
+      resp.end(
         JSON.stringify({
           users: [{
                 name: "Reinaldo Ramos",
@@ -63,21 +63,20 @@ let servidor = http.createServer(function (req, resp) {
         })
       );
 
-    /*
+    
       default:
-          break;
-          */
+        resp.end(`
+        <html>
+           <head>
+               <meta charset="utf -8">
+           </head>
+           <body>
+               <h1> Casa do Codigo </h1>
+           </body>
+        </html>
+   `);
   }
-  resp.end(`
-      <html>
-         <head>
-             <meta charset="utf -8">
-         </head>
-         <body>
-             <h1> Casa do Codigo </h1>
-         </body>
-      </html>
- `);
+ 
 });
 servidor.listen(3000, () => {
   console.log("funfando");
