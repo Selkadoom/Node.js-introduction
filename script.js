72d90cf8dbcb37f395464e0c9f30f71fd2a564d2
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-02-10 20:24:26 -0300

    10/02 - 20:23 - USANDO O APP E DEIXANDO O CODIGO MAIS PRÁTICO - [Aqui eu substitui os switches e console logs pelo app com express, dessa maneira, tudo que eu precisei fazer foi usar o método nativo get para pegar as rotas e com base nelas executar alguma arrow function, dessa maneira, o código ficou muito menor e prático]

--- script.js
+++ script.js
@@ -2,53 +2,36 @@ const express = require("express");
 
 let app = express();
 
-app.get(function (req, resp) {
-  console.log("URL:", req.url); //todas as solicitações do servidor são mandadas pro req e a reposta pro res
-  console.log("METHOD:", req.method);
-  switch (req.url) {
-    case "/":
-      req.statusCode = 200; //quando o usuário conecta com o servidor ele retorna um status code
-      //se for 200, a conexão deu certo
-      resp.setHeader("Content-Type", "text/html"); //Aqui eu to passando o tipo do conteudo
-      //e avisando que é Html
-      //----------
+app.get("/", (req, resp) => {
+  req.statusCode = 200; //quando o usuário conecta com o servidor ele retorna um status code
+  //se for 200, a conexão deu certo
+  resp.setHeader("Content-Type", "text/html"); //Aqui eu to passando o tipo do conteudo
+  //e avisando que é Html
+  //----------
 
-      resp.end("<h1>Ola</h1>"); //pedindo pra mandar uma res que cria um h1 com Ola escito
-
-      break;
-
-    case "/users":
-      req.statusCode = 200; //quando o usuário conecta com o servidor ele retorna um status code
-      //se for 200, a conexão deu certo
-      resp.setHeader("Content-Type", "application/json"); //Aqui eu to passando o tipo do conteudo
-      //e avisando que é Html
-      //----------
-
-      resp.end(
-        JSON.stringify({
-          users: [
-            {
-              name: "Reinaldo Ramos",
-              email: "Reinaldo_RAmos@gmail.com",
-              id: 1,
-            },
-          ],
-        })
-      );
-
-    default:
-      resp.end(`
-        <html>
-           <head>
-               <meta charset="utf -8">
-           </head>
-           <body>
-               <h1> Casa do Codigo </h1>
-           </body>
-        </html>
-   `);
-  }
+  resp.end("<h1>Ola</h1>"); //pedindo pra mandar uma res que cria um h1 com Ola escito
 });
-servidor.listen(3000, () => {
+
+app.get("/users", (req, resp) => {
+  req.statusCode = 200; //quando o usuário conecta com o servidor ele retorna um status code
+  //se for 200, a conexão deu certo
+  resp.setHeader("Content-Type", "application/json"); //Aqui eu to passando o tipo do conteudo
+  //e avisando que é Html
+  //----------
+
+  resp.end(
+    JSON.stringify({
+      users: [
+        {
+          name: "Reinaldo Ramos",
+          email: "Reinaldo_RAmos@gmail.com",
+          id: 1,
+        },
+      ],
+    })
+  );
+});
+
+app.listen(3000, () => {
   console.log("funfando");
 });
