edf2172cef6d9c25a24ddd82deb6e7527c70d024
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-02-10 17:38:03 -0300

    10/02 -- 17:35 - [Aqui eu comecei a criar um case que para pegar outro tipo de HTML, uma boa nota é a de que o Node.Js é uma linguagem bem comportamental, ou seja, funciona com base em comportamentos específicos, e nós podemos definir como ele se comporta perante cada um desses, nesse caso, caso ele receba a url com user, ao invés de retornar um header em HTML ele tem que retornar como reposta um JSON, no caso, um objeto user]

--- script.js
+++ script.js
@@ -28,27 +28,45 @@ server.listen(3000, '10.0.1.112', () => {
 
 */
 //ACIMA SÓ TEM OS ERRADOS
-const http = require('http');
+const http = require("http");
 
 let servidor = http.createServer(function (req, resp) {
-    console.log("URL:", req.url); //todas as solicitações do servidor são mandadas pro req e a reposta pro res
-    console.log("METHOD:", req.method);
+  console.log("URL:", req.url); //todas as solicitações do servidor são mandadas pro req e a reposta pro res
+  console.log("METHOD:", req.method);
   switch (req.url) {
-      case  '/':
-
+    case "/":
       req.statusCode = 200; //quando o usuário conecta com o servidor ele retorna um status code
       //se for 200, a conexão deu certo
-          res.setHeader('Content Type', 'text/html'); //Aqui eu to passando o tipo do conteudo
-          //e avisando que é Html
-          //----------
-          
-          res.end('<h1>Ola</h1>');//pedindo pra mandar uma res que cria um h1 com Ola escito
-        
-          
-          break;
-  
+      res.setHeader("Content-Type", "text/html"); //Aqui eu to passando o tipo do conteudo
+      //e avisando que é Html
+      //----------
+
+      res.end("<h1>Ola</h1>"); //pedindo pra mandar uma res que cria um h1 com Ola escito
+
+      break;
+
+    case "/users":
+      req.statusCode = 200; //quando o usuário conecta com o servidor ele retorna um status code
+      //se for 200, a conexão deu certo
+      res.setHeader("Content-Type", "application/json"); //Aqui eu to passando o tipo do conteudo
+      //e avisando que é Html
+      //----------
+
+      res.end(
+        JSON.stringify({
+          users: [{
+                name: "Reinaldo Ramos",
+                email: "Reinaldo_RAmos@gmail.com",
+                id: 1
+
+          }]
+        })
+      );
+
+    /*
       default:
           break;
+          */
   }
   resp.end(`
       <html>
@@ -59,10 +77,8 @@ let servidor = http.createServer(function (req, resp) {
              <h1> Casa do Codigo </h1>
          </body>
       </html>
- ` );
-
+ `);
 });
-servidor.listen(3000, ()=> {
-
-    console.log("funfando")
+servidor.listen(3000, () => {
+  console.log("funfando");
 });
