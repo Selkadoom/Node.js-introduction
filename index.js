bf0980cb0ce4a8b3a9443e6aa8eefb042a832ef8
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-02-15 18:45:16 -0300

    18:44 15/02 - Tentando arrumar o erro, basicamente o servidor nao incicia

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express');
-let RoutesIndex = require('./ROUTES/index');
-let routesUsers = require('./ROUTES/users')
+const express = require("express");
+let RoutesIndex = require("./ROUTES/index");
+let routesUsers = require("./ROUTES/users");
 let app = express();
+app.use(routesUsers);
+app.use(RoutesIndex);
 
-
-app.listen(3000, () => {
+app.listen(3000, "10.0.1.112", () => {
   console.log("funfando");
 });
