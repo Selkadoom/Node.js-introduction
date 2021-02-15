42ac4f749f26dbf972e525b5277c693e4de311bb
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-02-15 16:57:25 -0300

    15/02 16:56 - Chamando o express - [Aqui eu chamei a biblioteca express, coloquei uma let routes para receber a mesma, em seguida mandei os modulos para dentro de routes]

--- ROUTES/index.js
+++ ROUTES/index.js
@@ -1,4 +1,6 @@
-app.get("/", (req, resp) => {
+let express = require('express'); //pega o express
+let routes = express.Router();
+routes.get("/", (req, resp) => { //pega os métodos na biblioteca express
     req.statusCode = 200; //quando o usuário conecta com o servidor ele retorna um status code
     //se for 200, a conexão deu certo
     resp.setHeader("Content-Type", "text/html"); //Aqui eu to passando o tipo do conteudo
@@ -7,3 +9,5 @@ app.get("/", (req, resp) => {
   
     resp.end("<h1>Ola</h1>"); //pedindo pra mandar uma res que cria um h1 com Ola escito
   });
+
+  module.exports = routes; //exporta o modulo para as rotsas
